d718bcd5a4e82a6da4afc7669defe722931554f5
Pablo A <pablos.almi@gmail.com>
2022-08-08 20:31:33 -0300

    Se quitan annotations

--- src/main/java/com/servicio/app/productos/ServicioAppProductosApplication.java
+++ src/main/java/com/servicio/app/productos/ServicioAppProductosApplication.java
@@ -1,14 +1,9 @@
 package com.servicio.app.productos;
 
-import com.servicio.app.productos.config.SwaggerConfiguration;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-//@EnableEurekaClient
-@Import(SwaggerConfiguration.class)
-@EnableAutoConfiguration
+//@Import(SwaggerConfiguration.class)
 @SpringBootApplication
 public class ServicioAppProductosApplication {
 
